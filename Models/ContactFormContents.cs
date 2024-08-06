2c626ad970df5f57b39c424801e751282f737807
Asche Jurotich <asche.jurotich@gmail.com>
2024-08-05 21:59:00 -0500

    Add project files.

---
+++ Models/ContactFormContents.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Site.Models;
+
+public class ContactFormContents {
+
+	[Required(ErrorMessage = "Missing name.")]
+	public string Name { get; set; } = null!;
+
+	[Required(ErrorMessage = "Missing email.")]
+	[DataType(DataType.EmailAddress)]
+	public string Email { get; set; } = null!;
+
+	[Required(ErrorMessage = "Missing subject.")]
+	public string Subject { get; set; } = null!;
+
+	[Required(ErrorMessage = "Missing message.")]
+	[DataType(DataType.MultilineText)]
+	public string Message { get; set; } = null!;
+
+}
